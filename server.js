fae8509cefc9b8ce77710f7ce2e2f7ae8fc2762e
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-31 13:22:40 +0200

    HTML gerenderd d.m.v. Express

---
+++ server.js
@@ -0,0 +1,11 @@
+// Load Node.js modules.
+var express = require('express')
+
+// Initialise Express.
+var app = express()
+
+// Render static files.
+app.use(express.static('public'))
+
+// Set the port the website will run on.
+app.listen(8080)
