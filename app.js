c3369eff8dbe8f1114e986a2625628b71d091688
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-01 10:34:01 +0200

    app.js opgeruimd

--- app.js
+++ app.js
@@ -1,60 +1,48 @@
+// Import Express.
 import express from 'express'
+// Import Handlebars.
 import { engine } from 'express-handlebars'
 
+// Initialise Express.
 const app = express()
 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', './views')
-
+// Render static files.
 app.use(express.static('public'))
 
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded( { extended: true } ))
+// Set the view engine to Handlebars.
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
 
+// Parse incoming requests.
 app.use(express.urlencoded( { extended: true } ))
 
+// Set the port for Express.
+app.listen(3000)
+
+// Listen to all GET requests on /.
 app.get('/', (_req, res) => {
-    res.render('home')
+	// fs.readFile('informatie.json', 'utf8', function (err, data) {
+	// if (err) throw err;
+	// let info = JSON.parse(data);
+
+	res.render('home')
+
+	// res.render('home', {
+	// 	eerder_opgeslagen_data: info
+	// })
 })
-
-// Route. Luistert naar alle GET requests op /
-// app.get('/', (req, res) => {
-
-
-// 	fs.readFile('informatie.json', 'utf8', function (err, data) {
-// 	  if (err) throw err;
-// 	  let info = JSON.parse(data);
-
-
-//   		res.render('home', {
-//   			eerder_opgeslagen_data: info
-//   		})
-
-
-// 	});
-// })
 
 // let userInput;
 
-
+// Listen to all POST requests on /.
 app.post('/', (req, res) => {
-	console.log(req.body)
-
-    res.render('home', { resultaten: req.body })
-
-
 	// userInput = JSON.stringify(req.body.naam)
-
 
 	// fs.writeFile('informatie.json', userInput, 'utf8', cb => {
 		// console.log('werk dan');
 	// })
 
+	console.log(req.body)
 
-	// res.render('home', {
-	// 	gebruikersnaam: userInput
-	// })
+    res.render('home', { resultaten: req.body })	
 })
-
-app.listen(3000)
