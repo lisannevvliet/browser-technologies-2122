56057ae93c3ace374e7646f43fafa0b28814cd69
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-07 16:46:23 +0200

    README.md aangevuld en feature detection voor localStorage aangezet

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -8,48 +8,51 @@ window.onscroll = function() {
     $("#progress").style.width = (document.body.scrollTop || document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 100 + "%"
 }
 
-// Save the value of the input field in the localStorage.
-function save() {
-    localStorage.setItem(this.name, JSON.stringify({ [this.name]: this.value }))
-}
-
-// Save the name and student number.
-$("#naam").addEventListener("keyup", save)
-$("#studentnummer").addEventListener("keyup", save)
-
-// Fill in the name and student number from the localStorage, if there are any.
-try {
-    $("#naam").value = JSON.parse(localStorage.getItem("naam")).naam
-    $("#studentnummer").value = JSON.parse(localStorage.getItem("studentnummer")).studentnummer
-} catch { }
-
-// List of all number input fields.
-const elements = document.querySelectorAll("input[type=\"number\"]")
-
-// Validate almost all number input fields.
-elements.forEach((element, index) => {
-    // Skip the first number input field, because it contains the student number (which does not need to be validated).
-    if (index != 0) {
-        element.addEventListener("keyup", function() {
-            // Add a red outline if the number entered is not between 1 and 10.
-            if (this.value != "" && (this.value < 1 || this.value > 10)) {
-                this.classList.add("red")
-            } else {
-                this.classList.remove("red")
-            }
-        })
+// Detect whether localStorage is supported.
+if ("localStorage" in window && window["localStorage"] !== null) {
+    // Save the value of the input field in the localStorage.
+    function save() {
+        localStorage.setItem(this.name, JSON.stringify({ [this.name]: this.value }))
     }
-})
 
-// Clear the localStorage.
-function clear() {
-    localStorage.clear()
+    // Save the name and student number.
+    $("#naam").addEventListener("keyup", save)
+    $("#studentnummer").addEventListener("keyup", save)
+
+    // Fill in the name and student number from the localStorage, if there are any.
+    try {
+        $("#naam").value = JSON.parse(localStorage.getItem("naam")).naam
+        $("#studentnummer").value = JSON.parse(localStorage.getItem("studentnummer")).studentnummer
+    } catch { }
+
+    // List of all number input fields.
+    const elements = document.querySelectorAll("input[type=\"number\"]")
+
+    // Validate almost all number input fields.
+    elements.forEach((element, index) => {
+        // Skip the first number input field, because it contains the student number (which does not need to be validated).
+        if (index != 0) {
+            element.addEventListener("keyup", function() {
+                // Add a red outline if the number entered is not between 1 and 10.
+                if (this.value != "" && (this.value < 1 || this.value > 10)) {
+                    this.classList.add("red")
+                } else {
+                    this.classList.remove("red")
+                }
+            })
+        }
+    })
+
+    // Clear the localStorage.
+    function clear() {
+        localStorage.clear()
+    }
+
+    // List of all submit buttons.
+    const buttons = document.querySelectorAll("button[type=\"submit\"]")
+
+    // Clear the localStorage upon a click on one of the submit buttons.
+    buttons.forEach((element) => {
+        element.addEventListener("click", clear)
+    })
 }
-
-// List of all submit buttons.
-const buttons = document.querySelectorAll("button[type=\"submit\"]")
-
-// Clear the localStorage upon a click on one of the submit buttons.
-buttons.forEach((element) => {
-    element.addEventListener("click", clear)
-})
