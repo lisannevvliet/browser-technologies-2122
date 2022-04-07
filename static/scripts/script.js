e023c013aee018ec61b65ba49be8049844f1b258
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-07 14:26:58 +0200

    Progress bar en localStorage toegevoegd

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,13 @@
+// Return the Element object of the corresponding element.
+function $(element) {
+    return document.querySelector(element)
+}
+
+// When the user scrolls the page, update the progress bar.
+window.onscroll = function() {
+    $("#progress").style.width = (document.body.scrollTop || document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 100 + "%"
+}
+
 // Add a red outline if the number entered is not between 1 and 10.
 function validate() {
     if (this.value != "" && (this.value < 1 || this.value > 10)) {
@@ -17,3 +27,18 @@ elements.forEach((element, index) => {
         element.addEventListener("keyup", validate)
     }
 })
+
+// Save the value of the input field in the localStorage.
+function save() {
+    localStorage.setItem(this.name, JSON.stringify({ [this.name]: this.value }))
+}
+
+// Save the name and student number by adding event listeners.
+$("#naam").addEventListener("keyup", save)
+$("#studentnummer").addEventListener("keyup", save)
+
+// Fill in the name and student number from the localStorage, if there are any.
+try {
+    $("#naam").value = JSON.parse(localStorage.getItem("naam")).naam
+    $("#studentnummer").value = JSON.parse(localStorage.getItem("studentnummer")).studentnummer
+} catch { }
